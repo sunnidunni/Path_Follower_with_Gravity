1bbfd8e7549d86612e9a1bc7a863e0431d9905dd
Derek Sun <41048711+sunnidunni@users.noreply.github.com>
2023-12-21 15:21:37 +0800

    Add files via upload

---
+++ PathFollower.cs
@@ -0,0 +1,69 @@
+﻿
+using UnityEngine;
+using System;
+
+namespace PathCreation.Examples
+{
+    // Moves along a path at constant speed.
+    // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
+    public class PathFollower : MonoBehaviour
+    {
+        public PathCreator pathCreator;
+        public EndOfPathInstruction endOfPathInstruction;
+        public double speed;
+        double distanceTravelled;
+        public GameObject obj;
+        public double g;
+        double energy;
+        double v;
+        double prevx;
+        double prevy;
+        double dx;
+        double dy;
+        double Fn;
+        public double fric;
+
+
+
+        async void Start() {
+            if (pathCreator != null)
+            {
+                // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+                pathCreator.pathUpdated += OnPathChanged;
+                
+                energy = 57.9331 * g;
+                prevx = obj.transform.position.x;
+                prevy = obj.transform.position.y;
+
+            }
+        }
+
+        void Update()
+        {
+            if (pathCreator != null)
+            {
+                //dx = obj.transform.position.x - prevx;
+                //dy = obj.transform.position.y - prevy;
+                //Fn = g * ((dx / Math.Sqrt((Math.Pow(dx, 2) + Math.Pow(dy, 2)))));
+                //Debug.Log(dx);
+
+
+
+
+                v = speed+Math.Sqrt(2 * (energy - g * obj.transform.position.y));
+                distanceTravelled += v * Time.deltaTime;
+                transform.position = pathCreator.path.GetPointAtDistance((float)distanceTravelled, endOfPathInstruction);
+                transform.rotation = pathCreator.path.GetRotationAtDistance((float)distanceTravelled, endOfPathInstruction);
+                prevx = obj.transform.position.x;
+                prevy = obj.transform.position.y;
+
+            }
+        }
+
+        // If the path changes during the game, update the distance travelled so that the follower's position on the new path
+        // is as close as possible to its position on the old path
+        void OnPathChanged() {
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+    }
+}
